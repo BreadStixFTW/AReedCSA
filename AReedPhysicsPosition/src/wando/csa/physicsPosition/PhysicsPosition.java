30f73f7c84a2da4b16d8b8044cfa4a61a4656027
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2021-09-15 21:29:30 -0400

    Coded PhysicsPosition Project
    Coded the PhysicsPosition project for an assignment.

--- AReedPhysicsPosition/src/wando/csa/physicsPosition/PhysicsPosition.java
+++ AReedPhysicsPosition/src/wando/csa/physicsPosition/PhysicsPosition.java
@@ -1,6 +1,20 @@
 package wando.csa.physicsPosition;
 
+/*
+ * Programmer: Alex Reed
+ * Date: 9/15/2021
+ * Purpose: To calculate and print out the position of a body based off of the values given.
+ */
+
 public class PhysicsPosition {
-	public static double time = 0;
-	public static double initialPosition = 0;
+	public static void main(String[] args) {
+		double time = 5; // in seconds
+		double initialPosition = 10; // in meters
+		double initialVelocity = 15; // in meters per second
+		double rateOfAccel = 20; // in m/s^2
+		
+		double position = (initialPosition + initialVelocity) + (((1/2) * rateOfAccel) * (time * time));
+		
+		System.out.println("Position: " + position + " meters");
+	}
 }

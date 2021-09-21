dc817393d223d4795ff33f9f7b7da5358b9e72ec
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2021-09-21 10:00:32 -0400

    Added PokemonStats project
    Added the PokemonStats project.

---
+++ AReedPokemonStats/src/wando/csa/pokemonStats/PokemonStats.java
@@ -0,0 +1,11 @@
+package wando.csa.pokemonStats;
+
+/*
+ * Programmer: Alex Reed
+ * Date: 9/21/2021
+ * Purpose: To return a Pokemon's stats for Attack and HP at level 100.
+ */
+
+public class PokemonStats {
+
+}

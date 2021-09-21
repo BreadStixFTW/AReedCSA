c7b1c3cfa40510454786bf6099dab8ba891546c1
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2021-09-21 18:45:12 -0400

    Added Code for PokemonStats
    Completed the PokemonStats project.

--- AReedPokemonStats/src/wando/csa/pokemonStats/PokemonStats.java
+++ AReedPokemonStats/src/wando/csa/pokemonStats/PokemonStats.java
@@ -7,5 +7,23 @@ package wando.csa.pokemonStats;
  */
 
 public class PokemonStats {
-
+	public static int attackStat(int baseAttack) {
+		return baseAttack * 2 + 5;
+	}
+	
+	public static int hpStat(int baseHP) {
+		return 110 + baseHP * 2;
+	}
+	
+	public static void main(String[] args) {
+		// will be using Raichu's stats for this example
+		int attack = 90;
+		int hp = 60;
+		
+		int level100Attack = attackStat(attack);
+		int level100HP = hpStat(hp);
+		
+		System.out.println("Raichu's Level 100 Attack: " + level100Attack);
+		System.out.println("Raichu's Level 100 HP: " + level100HP);
+	}
 }

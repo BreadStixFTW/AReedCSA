51abc5b76ac7463d3fb1e3d07be0b3b522f35e2d
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2021-10-29 22:33:26 -0400

    Removed switch statement and replaced double array list with two separate array lists

--- AReedWarCardGame/src/wando/csa/warCardGame/WarCardGame.java
+++ AReedWarCardGame/src/wando/csa/warCardGame/WarCardGame.java
@@ -10,13 +10,15 @@ import java.util.*;
 
 public class WarCardGame {
 	public HashMap<String, Integer> deck;
-	public ArrayList<ArrayList<String>> playerDecks;
+	public ArrayList<String> playerDeck;
+	public ArrayList<String> opponentDeck;
 	public final String[] suits = {"Spades", "Clubs", "Hearts", "Diamonds"};
 	public final String[] cards = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
 	
 	public WarCardGame() {
 		deck = newDeck();
-		playerDecks = playerDecks();
+		playerDeck = splitDeck();
+		opponentDeck = splitDeck();
 	}
 	
 	// method that searches the suits of a specific card to see which ones haven't been instantiated
@@ -57,115 +59,80 @@ public class WarCardGame {
 		// while loop for adding the cards to the deck
 		// keeps looping until all 52 cards are in the deck
 		while (newDeck.size() < 52) {
+			// gets a number from 2 - 14, which are the numerical values of the cards
+			// from the two card to the ace
 			cardNum = card.nextInt(13) + 2;
 			
-			// switch statement with cases for values ranging from 2 - 14
-			// these are the card values that will be used in the game
-			switch (cardNum) {
-			case 2:
-				newCard = searchSuits(newDeck, cards[0], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 3:
-				newCard = searchSuits(newDeck, cards[1], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 4:
-				newCard = searchSuits(newDeck, cards[2], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 5:
-				newCard = searchSuits(newDeck, cards[3], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 6:
-				newCard = searchSuits(newDeck, cards[4], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 7:
-				newCard = searchSuits(newDeck, cards[5], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 8:
-				newCard = searchSuits(newDeck, cards[6], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 9:
-				newCard = searchSuits(newDeck, cards[7], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 10:
-				newCard = searchSuits(newDeck, cards[8], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 11:
-				newCard = searchSuits(newDeck, cards[9], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 12:
-				newCard = searchSuits(newDeck, cards[10], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 13:
-				newCard = searchSuits(newDeck, cards[11], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			case 14:
-				newCard = searchSuits(newDeck, cards[12], cardNum);
-				newDeck.put(newCard.getKey(), newCard.getValue());
-				break;
-			default:
-				// two is default card
-				newDeck.put("Two of Spades", 2);
-				break;
-			}
+			System.out.println(cardNum);
+			
+			// adds a new card to the deck
+			newCard = searchSuits(newDeck, cards[cardNum - 2], cardNum);
+			newDeck.put(newCard.getKey(), newCard.getValue());
 		}
+		System.out.println(newDeck);
 		return newDeck;
 	}
 	
 	// method that splits the card deck into two decks, one for each player
-	public ArrayList<ArrayList<String>> playerDecks() {
-		ArrayList<ArrayList<String>> playerDecks = new ArrayList();
-		playerDecks.add(new ArrayList());
-		playerDecks.add(new ArrayList());
+	// the first deck is the player's deck, and the second deck is the opponent's deck
+	public ArrayList<String> splitDeck() {
+		ArrayList<String> splitDeck = new ArrayList<String>();
+		
 		int count = 1;
 		
 		for (Map.Entry<String, Integer> m : deck.entrySet()) {
-			if (count <= 26) {
-				playerDecks.get(0).add(m.getKey());
+			if (count <= 26 && playerDeck == null) {
+				splitDeck.add(m.getKey());
 			}
-			else {
-				playerDecks.get(1).add(m.getKey());
+			else if (count >= 27 && playerDeck != null) {
+				splitDeck.add(m.getKey());
 			}
 			count++;
 		}
-		return playerDecks;
+		return splitDeck;
 	}
 	
 	// method that determines the winner of the current card battle
 	public void battle() {
 		// put print statements here that tell which current card each player has
+		System.out.println("Your current card is the " + playerDeck.get(playerDeck.size() - 1));
 		
+		System.out.println("Your opponent's card is the " + opponentDeck.get(opponentDeck.size() - 1));
 		// checks if the value of the current cards in both player's decks equal each other
 		// by getting the value from the current card's name
 		// via using the current card's name as a key for the deck hash map
-		if (deck.get(playerDecks.get(0).get(playerDecks.get(0).size() - 1)) == deck.get(playerDecks.get(1).get(playerDecks.get(1).size() - 1))) {
+		if (deck.get(playerDeck.get(playerDeck.size() - 1)) == deck.get(opponentDeck.get(opponentDeck.size() - 1))) {
 			// call war method
 		}
 		// if the current card of the first deck's value is greater than the other card's value,
+		// then the card you won with first gets moved to the bottom of the first deck,
 		// then the other card gets added to the bottom of the first deck
 		// and the other card also gets removed from the top of the second deck
-		else if (deck.get(playerDecks.get(0).get(playerDecks.get(0).size() - 1)) > deck.get(playerDecks.get(1).get(playerDecks.get(1).size() - 1))) {
-			playerDecks.get(0).add(playerDecks.get(1).get(playerDecks.get(1).size() - 1));
-			playerDecks.get(1).remove(playerDecks.get(1).size() - 1);
+		else if (deck.get(playerDeck.get(playerDeck.size() - 1)) > deck.get(opponentDeck.get(opponentDeck.size() - 1))) {
+			// adds winning card to bottom of winning player's deck
+			playerDeck.add(0, playerDeck.get(playerDeck.size() - 1));
+			playerDeck.remove(playerDeck.size() - 1);
 			
-			System.out.println("You won the " + playerDecks.get(0).get(playerDecks.get(0).size() - 1) + "!");
+			// adds opponent's card to the bottom of the winning player's deck
+			playerDeck.add(0, opponentDeck.get(opponentDeck.size() - 1));
+			opponentDeck.remove(opponentDeck.size() - 1);
+			
+			System.out.println("You won the " + playerDeck.get(0) + " from your opponent!");
 		}
+		// if the current card of the first deck's value is less than the other card's value,
+		// then the card your opponent won with first gets moved to the bottom of the second deck,
+		// then your card gets added to the bottom of the second deck
+		// and your card also gets removed from the top of the first deck
 		else {
-			playerDecks.get(1).add(playerDecks.get(0).get(playerDecks.get(0).size() - 1));
-			playerDecks.get(0).remove(playerDecks.get(0).size() - 1);
+			// adds winning card to bottom of winning player's deck
+			opponentDeck.add(0, opponentDeck.get(opponentDeck.size() - 1));
+			opponentDeck.remove(opponentDeck.size() - 1);
+						
+			// adds opponent's card to the bottom of the winning player's deck
+			opponentDeck.add(0, playerDeck.get(playerDeck.size() - 1));
+			playerDeck.remove(playerDeck.size() - 1);
 			
-			System.out.println("You lost your " + playerDecks.get(1).get(playerDecks.get(1).size() - 1) + "!");
+			System.out.println("You lost your " + opponentDeck.get(0) + " to the opponent!");
 		}
 	}
 	
@@ -186,19 +153,20 @@ public class WarCardGame {
 //			System.out.println(war.playerDecks.get(1).get(i));
 //		}
 		boolean decMade = false;
-		while (war.playerDecks.get(0).size() > 0 && war.playerDecks.get(1).size() > 0) {
-			System.out.println("You have " + war.playerDecks.get(0).size() + " cards.");
+		
+		while (war.playerDeck.size() > 0 && war.opponentDeck.size() > 0) {
+			System.out.println("You have " + war.playerDeck.size() + " cards.");
 			
 			System.out.println("Type \"flip\" to flip your card.");
 			String response = input.next();
 			do {
 				switch(response) {
-				case "input":
+				case "flip":
 					war.battle();
 					decMade = true;
 					break;
 				default:
-					
+					System.out.println("Please enter \"flip\"");
 				}
 			} while(!decMade);
 		}

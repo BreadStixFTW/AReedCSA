729e431c21b0a79605833a8b01d3c34e9d6a6cc4
BreadStixFTW <77415101+BreadStixFTW@users.noreply.github.com>
2022-05-24 23:45:43 -0400

    Started coding main game logic

--- AReedCupShuffle/src/cupShuffle/common/Game.java
+++ AReedCupShuffle/src/cupShuffle/common/Game.java
@@ -1,5 +1,26 @@
 package cupShuffle.common;
 
 public class Game {
-
+	private boolean[] cups;
+	private int score;
+	
+	public Game() {
+		this.cups = new boolean[3];
+		
+		cups[0] = false;
+		cups[1] = true;
+		cups[2] = false;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public boolean[] getCups() {
+		return this.cups;
+	}
+	
+	public int getScore() {
+		return this.score;
+	}
 }
--- AReedCupShuffle/src/cupShuffle/views/MainWindow.java
+++ AReedCupShuffle/src/cupShuffle/views/MainWindow.java
@@ -1,31 +1,48 @@
 package cupShuffle.views;
 
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
+
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JPanel;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingUtilities;
 import javax.swing.JLayeredPane;
 import javax.swing.JLabel;
-
-import cupShuffle.resources.*;
 import javax.swing.ImageIcon;
 
+import cupShuffle.common.Game;
+import java.util.*;
+import java.lang.Thread;
+import java.lang.reflect.InvocationTargetException;
+
 public class MainWindow {
+	private Game game;
 	
 	private JFrame frame;
-	private JButton btnNewButton;
+	private JButton startBtn;
 	private JPanel startPanel;
 	private JPanel gamePanel;
 	private JLabel scoreLabel;
 	private JButton guessBtn1;
 	private JButton guessBtn2;
 	private JButton guessBtn3;
+	private JLayeredPane layeredPane_1;
+	private JLabel cupImg1;
+	private JLabel cupImg2;
+	private JLabel cupImg3;
+	private JLabel marbleImg1;
+	private JLabel marbleImg2;
+	private JLabel marbleImg3;
+	private JButton exitBtn;
+	private JButton shuffleBtn;
 
 	/**
 	 * Launch the application.
@@ -49,6 +66,8 @@ public class MainWindow {
 	public MainWindow() {
 		createComponents();
 		createEvents();
+		
+		game = new Game();
 	}
 
 	/**
@@ -73,97 +92,208 @@ public class MainWindow {
 		);
 		
 		startPanel = new JPanel();
-		startPanel.setBounds(0, 0, 14, 12);
+		startPanel.setBounds(0, 0, 434, 260);
 		layeredPane.add(startPanel);
 		
-		btnNewButton = new JButton("Start!");
-		btnNewButton.setBackground(Color.GREEN);
+		startBtn = new JButton("Start!");
+		startBtn.setBackground(Color.GREEN);
 		GroupLayout gl_startPanel = new GroupLayout(startPanel);
 		gl_startPanel.setHorizontalGroup(
 			gl_startPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_startPanel.createSequentialGroup()
 					.addGap(186)
-					.addComponent(btnNewButton)
+					.addComponent(startBtn)
 					.addContainerGap(187, Short.MAX_VALUE))
 		);
 		gl_startPanel.setVerticalGroup(
 			gl_startPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(Alignment.TRAILING, gl_startPanel.createSequentialGroup()
 					.addContainerGap(128, Short.MAX_VALUE)
-					.addComponent(btnNewButton)
+					.addComponent(startBtn)
 					.addGap(109))
 		);
 		startPanel.setLayout(gl_startPanel);
 		
 		gamePanel = new JPanel();
-		gamePanel.setVisible(false);
 		gamePanel.setBounds(0, 0, 434, 260);
 		layeredPane.add(gamePanel);
 		
-		scoreLabel = new JLabel("Score: 0");
-		
-		guessBtn1 = new JButton("Guess!");
-		guessBtn1.setVisible(false);
-		
-		guessBtn2 = new JButton("Guess!");
-		guessBtn2.setVisible(false);
-		
-		guessBtn3 = new JButton("Guess!");
-		guessBtn3.setVisible(false);
-		
-		JButton shuffleBtn = new JButton("Shuffle!");
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/cup.png")));
+		layeredPane_1 = new JLayeredPane();
 		GroupLayout gl_gamePanel = new GroupLayout(gamePanel);
 		gl_gamePanel.setHorizontalGroup(
 			gl_gamePanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_gamePanel.createSequentialGroup()
-					.addContainerGap(357, Short.MAX_VALUE)
-					.addComponent(scoreLabel, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
-				.addGroup(gl_gamePanel.createSequentialGroup()
-					.addGap(75)
-					.addComponent(guessBtn1)
-					.addGap(40)
-					.addComponent(guessBtn2)
-					.addGap(47)
-					.addComponent(guessBtn3)
-					.addContainerGap(83, Short.MAX_VALUE))
-				.addGroup(gl_gamePanel.createSequentialGroup()
-					.addGap(167)
-					.addComponent(shuffleBtn, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(185, Short.MAX_VALUE))
-				.addGroup(gl_gamePanel.createSequentialGroup()
-					.addGap(84)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(241, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, gl_gamePanel.createSequentialGroup()
+					.addComponent(layeredPane_1, GroupLayout.PREFERRED_SIZE, 434, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_gamePanel.setVerticalGroup(
 			gl_gamePanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_gamePanel.createSequentialGroup()
-					.addComponent(scoreLabel, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
-					.addGap(43)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 400, Short.MAX_VALUE)
-					.addGroup(gl_gamePanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(guessBtn2)
-						.addComponent(guessBtn1)
-						.addComponent(guessBtn3))
-					.addGap(18)
-					.addComponent(shuffleBtn)
-					.addGap(39))
+				.addComponent(layeredPane_1, GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE)
 		);
+		
+		guessBtn3 = new JButton("Guess!");
+		guessBtn3.setBounds(178, 148, 63, 21);
+		layeredPane_1.add(guessBtn3);
+		
+		guessBtn2 = new JButton("Guess!");
+		guessBtn2.setBounds(285, 148, 63, 21);
+		layeredPane_1.add(guessBtn2);
+		
+		shuffleBtn = new JButton("Shuffle!");
+		shuffleBtn.setBounds(169, 197, 82, 21);
+		layeredPane_1.add(shuffleBtn);
+		
+		guessBtn1 = new JButton("Guess!");
+		guessBtn1.setBounds(74, 148, 63, 21);
+		layeredPane_1.add(guessBtn1);
+		
+		scoreLabel = new JLabel("Score: 0");
+		scoreLabel.setBounds(378, 0, 56, 39);
+		layeredPane_1.add(scoreLabel);
+		
+		cupImg1 = new JLabel("");
+		cupImg1.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/cup.png")));
+		cupImg1.setBounds(74, 54, 63, 84);
+		layeredPane_1.add(cupImg1);
+		
+		cupImg2 = new JLabel("");
+		cupImg2.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/cup.png")));
+		cupImg2.setBounds(178, 54, 63, 85);
+		layeredPane_1.add(cupImg2);
+		
+		cupImg3 = new JLabel("");
+		cupImg3.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/cup.png")));
+		cupImg3.setBounds(285, 54, 63, 84);
+		layeredPane_1.add(cupImg3);
+		
+		marbleImg1 = new JLabel("");
+		marbleImg1.setVisible(false);
+		marbleImg1.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/marble.png")));
+		marbleImg1.setBounds(87, 83, 38, 29);
+		layeredPane_1.add(marbleImg1);
+		
+		marbleImg2 = new JLabel("");
+		marbleImg2.setVisible(false);
+		marbleImg2.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/marble.png")));
+		marbleImg2.setBounds(191, 83, 38, 29);
+		layeredPane_1.add(marbleImg2);
+		
+		marbleImg3 = new JLabel("");
+		marbleImg3.setVisible(false);
+		marbleImg3.setIcon(new ImageIcon(MainWindow.class.getResource("/cupShuffle/resources/marble.png")));
+		marbleImg3.setBounds(298, 83, 38, 29);
+		layeredPane_1.add(marbleImg3);
+		
+		exitBtn = new JButton("Exit");
+		exitBtn.setBackground(Color.RED);
+		exitBtn.setBounds(349, 239, 85, 21);
+		layeredPane_1.add(exitBtn);
+		guessBtn1.setVisible(false);
+		guessBtn2.setVisible(false);
+		guessBtn3.setVisible(false);
 		gamePanel.setLayout(gl_gamePanel);
 		frame.getContentPane().setLayout(groupLayout);
 	}
 	
 	private void createEvents() {
-		btnNewButton.addActionListener(new ActionListener() {
+		startBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				startPanel.setVisible(false);
 				gamePanel.setVisible(true);
 			}
 		});
+		
+		exitBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gamePanel.setVisible(false);
+				startPanel.setVisible(true);
+			}
+		});
+		
+		shuffleBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shuffleCups();
+			}
+		});
+	}
+	
+	public void shuffleCups() {
+		marbleImg2.setVisible(true);
+		
+		Runnable uiUpdate = new Runnable() {
+			public void run() {
+				System.out.println("Updating...");
+				
+				cupImg2.setLocation(cupImg2.getX(), 25);
+			}
+		};
+		
+		Thread uiThread = new Thread() {
+			public void run() {
+				try {
+					SwingUtilities.invokeAndWait(uiUpdate);
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				waitMillis(3000);
+				
+				System.out.println("Updated!");
+			}
+		};
+		
+		uiThread.start();
+		
+		try {
+			uiThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		while (cupImg2.getY() > 25) {
+//			cupImg2.setLocation(cupImg2.getX(), cupImg2.getY() - 1);
+//			
+//			System.out.println("Test");
+//			
+//			waitMillis(1000);
+//		}
+//		
+//		while (cupImg2.getY() < 54) {
+//			cupImg2.setLocation(cupImg2.getX(), cupImg2.getY() + 1);
+//			
+//			waitMillis(1000);
+//		}
+		
+//		
+//		for (int i = 0; i < 10; i++) {
+//			int cupOne = (int) ((Math.random() * 3) + 1);
+//			int cupTwo = (int) ((Math.random() * 3) + 1);
+//			
+//			while (cupTwo == cupOne) {
+//				cupTwo = (int) ((Math.random() * 3) + 1);
+//			}
+//			
+//			JLabel labelOne = null;
+//			JLabel labelTwo = null;
+//			
+//			if (cupOne == 1) {
+//				labelOne = cupImg1;
+//			}
+//			else if (cupOne == 2) {
+//				labelTwo = cupImg2;
+//			}
+//		}
+	}
+	
+	public void waitMillis(long millis) {
+		long currTime = System.currentTimeMillis();
+		long waitTime = currTime + millis;
+		
+		while (currTime < waitTime) {
+			currTime = System.currentTimeMillis();
+		}
 	}
 }
